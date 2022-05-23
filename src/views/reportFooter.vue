040ed383fc7858cf4be6a3b8d780d0355ee2f8bc
Mohamed Hassan <45787392+m019hassan@users.noreply.github.com>
2022-05-23 10:02:03 +0200

    update

--- src/views/reportFooter.vue
+++ src/views/reportFooter.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="footer-info mt-3">
-    <h3>this is Footer</h3>
-    <p>Lorem ipsum dolor sit amet.</p>
+    <h3>© وزارةالثقافة 2021 | سري للغاية| 1
+      <span></span>
+    </h3>
+    <!-- <p>Lorem ipsum dolor sit amet.</p> -->
   </div>
 </template>
 <script>
@@ -9,3 +11,40 @@ export default {
   name: "ReportFooter",
 };
 </script>
+
+<style scoped>
+.footer-info{
+  
+}
+
+h3{
+  position: fixed;
+  bottom: -25px;
+  left: 0rem;
+  z-index: 999;
+  height: 35px;
+  margin: 0 1rem 0.25rem;
+  font-size: 12px;
+  /* position: relative; */
+  width: fit-content;
+}
+
+h3::after{
+  content: '';
+  width: calc(100% - 30px);
+  height: 5px;
+  background-color: orange !important;
+  -webkit-print-color-adjust: exact; 
+  position: absolute;
+  top: -0.75rem;
+  left: -1rem;
+}
+h3 span::after{
+  /* display: inline-block; */
+  color: red;
+  counter-increment: page;
+  /* content: counter(page); */
+  margin: 0 0.25rem;
+}
+
+</style>
--- src/views/reportHeader.vue
+++ src/views/reportHeader.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
-    <div class="container-fluid mb-3">
-      <div class="row justify-content-between">
-        <div class="col-8">
-          <h4>Title Report</h4>
-          <p><strong>Description:</strong> <br> Lorem ipsum dolor sit amet.</p>
+    <div class="container-fluid">
+      <div class="row justify-content-between align-items-center">
+        <div class="col-3 d-flex">
+          <!-- https://via.placeholder.com/80x80 -->
+            <img class="img-fluid " src="../assets/logo.png" alt="" />
         </div>
-        <div class="col-4 d-flex">
-            <img class="img-thumbnail ms-auto " src="https://via.placeholder.com/80x80" alt="" />
+        <div class="col-9 text-end">
+          <h1 class="text-right">
+            <span class="text-dark">GCA02(2/2)  - </span>
+            تطوير الملكتبات العامة
+          </h1>
+          <!-- <p><strong>Description:</strong> <br> Lorem ipsum dolor sit amet.</p> -->
         </div>
+        
       </div>
     </div>
   </div>
@@ -21,7 +26,21 @@ export default {
 
 <style scoped>
 img{
-    width: 80px;
-    height: 80px;
+    /* width: 80px; */
+    height: 120px;
+}
+h1{
+  font-size: 28px;
+  position: relative;
+}
+h1::after{
+  content: '';
+  width: 80px;
+  height: 7px;
+  background-color: orange !important;
+  -webkit-print-color-adjust: exact; 
+  position: absolute;
+  bottom: -1rem;
+  right: 0;
 }
 </style>
